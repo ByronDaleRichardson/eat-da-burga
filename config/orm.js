5ef3a292a15a94cc6ed2a8a26880229ae09ace34
Byron Richardson <Byron Richardson>
2017-04-05 10:19:51 -0400

    more

---
+++ config/orm.js
@@ -0,0 +1,63 @@
+// import MySQL connection.
+var connection = require('../config/connection.js');
+
+// helper function for SQL syntax.
+function printQuestionMarks(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push('?')
+	};
+	return arr.toString();
+};
+
+// helper function for SQL syntax.
+function objToSql(ob){
+	var arr = [];
+	for (var key in ob) {
+		arr.push(key + '=' + ob[key]);
+	};
+	return arr.toString();
+};
+
+// Object for all our SQL statement functions.
+var orm = {
+	all: function(tableInput, cb) {
+		var queryString = 'SELECT * FROM ' = tableInput;
+
+		connection.query(queryString, function(err, result){
+			if(err) throw err;
+			cb(result);
+		});
+	},
+	create: function(table, col, vals, cb) {
+		var queryString = 'INSERT INTO ' + table;
+		queryString = queryString + ' (';
+		queryString = queryString + col.toString();
+		queryString = queryString + ') ';
+		queryString = queryString + 'VALUES (';
+		queryString = queryString + printQestionMarks(vals.length);
+		queryString = queryString + ') ';
+
+		connection.query(queryString, vals, function(err, reslut) {
+			if(err) throw err;
+			cb(result);
+		});
+	}.
+	update: function(table, objColVals, condition, cb) {
+		var queryString = 'UPDATE ' + table;
+		queryString = queryString + ' SET ';
+		queryString = queryString + objToSql(objColVals);
+		queryString = queryString + ' WHERE ';
+		queryString = queryString + condition;
+
+		console.log(queryString);
+
+		connection.query(queryString, function(err, result) {
+			if(err) throw err;
+			cb(result);
+		});
+	}
+};
+
+// Export the orm object for the model.
+module.exports = orm;
