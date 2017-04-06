14b9d5bbcff779f5dd8804d95a9fc74a2060ec6c
ByronDaleRichardson <musicmanx@gmail.com>
2017-04-06 12:22:38 -0400

    added reorder btn

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection; /*= mysql.createConnection({
     host     : 'us-cdbr-iron-east-03.cleardb.net',
     user     : 'beae2059bf3c81',
     password : '23b497bc',
     database : 'heroku_1eae2da06330d06'
-});
+});*/
 
-/*if(process.env.JAWSDB_URL) {
+if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
         password: '1choraliers2',
         database: 'burgers_db',
     });
-};*/
+};
 
 connection.connect(function(err) {
     if (err) {
