fe6e6ecc6d73c6132a72a3ae6c62f0f1761dd323
Byron Richardson <Byron Richardson>
2017-04-06 18:12:05 -0400

    took out clearDB, works when run locally. not pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+/* var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-03.cleardb.net',
     user     : 'beae2059bf3c81',
     password : '23b497bc',
     database : 'heroku_1eae2da06330d06'
-});
+});  */
 
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     root: 3000,
     host: 'localhost',
     user: 'root',
     password: '1choraliers2',
     database: 'burgers_db',
-}); */
+});
 
 
 connection.connect(function(err) {
