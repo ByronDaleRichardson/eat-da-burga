f8e74f34742248923ed2ad58a56eac3021f40b34
Byron Richardson <Byron Richardson>
2017-04-06 17:51:42 -0400

    trying to get clear db working

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
 var mysql = require('mysql');
-var connection; /*= mysql.createConnection({
+
+var connection = mysql.createConnection({
     host     : 'us-cdbr-iron-east-03.cleardb.net',
     user     : 'beae2059bf3c81',
     password : '23b497bc',
     database : 'heroku_1eae2da06330d06'
-});*/
+});
 
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-    connection = mysql.createConnection({
-        root: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: '1choraliers2',
-        database: 'burgers_db',
-    });
-};
+
+/*var connection = mysql.createConnection({
+    root: 3000,
+    host: 'localhost',
+    user: 'root',
+    password: '1choraliers2',
+    database: 'burgers_db',
+}); */
+
 
 connection.connect(function(err) {
     if (err) {
@@ -27,13 +26,3 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
-
-/*mysql://beae2059bf3c81:23b497bc@us-cdbr-iron-east-03.cleardb.net/
-heroku_1eae2da06330d06?reconnect=true
-
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-03.cleardb.net',
-  user     : 'beae2059bf3c81',
-  password : '23b497bc',
-  database : 'heroku_1eae2da06330d06'
-});*/
