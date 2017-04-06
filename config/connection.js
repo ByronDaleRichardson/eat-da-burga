c6adf66562d7c556c46b70423a84727b6cb527eb
ByronDaleRichardson <musicmanx@gmail.com>
2017-04-05 21:52:56 -0400

    clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 var mysql = require('mysql');
-var connection;
+var connection = mysql.createConnection({
+    host     : 'us-cdbr-iron-east-03.cleardb.net',
+    user     : 'beae2059bf3c81',
+    password : '23b497bc',
+    database : 'heroku_1eae2da06330d06'
+});
 
-if(process.env.JAWSDB_URL) {
+/*if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
@@ -11,7 +16,7 @@ if(process.env.JAWSDB_URL) {
         password: '1choraliers2',
         database: 'burgers_db',
     });
-};
+};*/
 
 connection.connect(function(err) {
     if (err) {
@@ -22,3 +27,13 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
+/*mysql://beae2059bf3c81:23b497bc@us-cdbr-iron-east-03.cleardb.net/
+heroku_1eae2da06330d06?reconnect=true
+
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-03.cleardb.net',
+  user     : 'beae2059bf3c81',
+  password : '23b497bc',
+  database : 'heroku_1eae2da06330d06'
+});*/
