2b66c8b8896810a0d1ea6c5ea1fc45e81e2373e2
Byron Richardson <Byron Richardson>
2017-04-06 21:35:13 -0400

    fixed clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,26 @@
 var mysql = require('mysql');
 
-/* var connection = mysql.createConnection({
+var connection = mysql.createPool({
     host     : 'us-cdbr-iron-east-03.cleardb.net',
     user     : 'beae2059bf3c81',
     password : '23b497bc',
     database : 'heroku_1eae2da06330d06'
-});  */
+});
 
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     root: 3000,
     host: 'localhost',
     user: 'root',
     password: '1choraliers2',
     database: 'burgers_db',
-});
+}); */
 
 
-connection.connect(function(err) {
+connection.getConnection(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
+        connection.release();
         return;
     }
     console.log('connected as id ' + connection.threadId);
